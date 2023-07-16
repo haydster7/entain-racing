4529910c8c5b8021ed8f7b696f7b7c2b84242b8a
Hayden Moulds <haydster7@gmail.com>
2023-07-16 16:07:08 +1000

    Issue #1 Implement visibility filter in racing service (#2)
    * i-1 implement visibility filter
    
    * update dependencies to support proto optional
    
    * Explcit experimental optional flag for ci

--- racing/db/races.go
+++ racing/db/races.go
@@ -2,11 +2,12 @@ package db
 
 import (
 	"database/sql"
-	"github.com/golang/protobuf/ptypes"
-	_ "github.com/mattn/go-sqlite3"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	_ "github.com/mattn/go-sqlite3"
 
 	"git.neds.sh/matty/entain/racing/proto/racing"
 )
@@ -77,6 +78,12 @@ func (r *racesRepo) applyFilter(query string, filter *racing.ListRacesRequestFil
 		for _, meetingID := range filter.MeetingIds {
 			args = append(args, meetingID)
 		}
+	}
+
+	//Only apply visibility filter if it is defined, to avoid default value of false
+	if filter.Visible != nil {
+		clauses = append(clauses, "visible = ?")
+		args = append(args, filter.GetVisible())
 	}
 
 	if len(clauses) != 0 {
